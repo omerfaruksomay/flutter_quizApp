1b11b58c9ca00b7969d9391a9f52a048fdadb6b4
omerfaruksomay <omersomay@stu.aydin.edu.tr>
2023-06-21 16:04:33 +0300

    21.06.2023

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:quizapp/start_section.dart';
+import 'package:quizapp/quiz.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
-      home: Scaffold(
-        body: StartScreen(),
-      ),
-    ),
-  );
+  runApp(const Quiz());
 }
